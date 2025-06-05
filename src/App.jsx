e2cde0ed2718b0a3961333c326514124ceb8603e
dymasyovie333 <yoviedymas@gmail.com>
2025-06-05 21:04:02 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="p-4 text-center text-xl font-bold">
+      Rapot Absensi App (Vite + React + Tailwind)
+    </div>
+  );
+}
